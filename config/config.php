d54c4763427fd213ffcd1121de83ff4d69005017
Marko Cupic <m.cupic@gmx.ch>
2016-10-15 12:11:23 +0200

    dev

--- config/config.php
+++ config/config.php
@@ -10,5 +10,8 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
+// Keep Backup Files 30 d on the server
+$GLOBALS['TL_CONFIG']['dbBackup']['keepBackupFiles'] = 30;
 
 $GLOBALS['TL_HOOKS']['generatePage'][] = array('MCupic\DbBackup', 'doDbBackup');
+
